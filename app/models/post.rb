155ef0f9c8f33b23e6dfdce863d4ef579283bc46
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2024-08-31 23:55:18 +0100

    Improve step validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,26 @@
 class Post < ApplicationRecord
-  validates :published_at, presence: true
+  validates :title, :description, :published_at, presence: true
 
   def step_valid?(step)
     case step
     when 1
-      errors.add(:title, "can't be blank") and return false unless title.present?
+      valid_attributes?(:title)
     when 2
-      errors.add(:description, "can't be blank") and return false unless description.present?
+      valid_attributes?(:title, :description)
     else
-      return false
+      super
+    end
+  end
+
+  def valid_attributes?(*attributes)
+    self.errors.clear
+
+    attributes.each do |attribute|
+      self.class.validators_on(attribute).each do |validator|
+        validator.validate_each(self, attribute, send(attribute))
+      end
     end
 
-    true
+    errors.none?
   end
 end
